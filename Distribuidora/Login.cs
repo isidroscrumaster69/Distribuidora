24916f54bf36212e8c9002bbd458fe6adc00ffca
angel <angelchuy153@gmail.com>
2022-04-02 00:44:33 -0600

    loginactualizdo

--- Distribuidora/Login.cs
+++ Distribuidora/Login.cs
@@ -15,11 +15,10 @@ namespace Distribuidora
 {
     public partial class Login : Form
     {
-        SqlConnection con = new SqlConnection();
+      
         public Login()
         {
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = "Data Source=MANDO-NOTEBOOK;Initial Catalog=distribuidora;Integrated Security=True";
+            
             InitializeComponent();
         }
 
@@ -27,17 +26,16 @@ namespace Distribuidora
         {
 
         }
-
+        conexionbd con = new conexionbd();
         private void btnIngresar_Click(object sender, EventArgs e)
         {
+           
             var menu = new Menu();
 
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = "Data Source=MANDO-NOTEBOOK;Initial Catalog=distribuidora;Integrated Security=True";
-            con.Open();
+            con.abrir();
             string user = txtUsername.Text;
             string password = txtPassword.Text;
-            SqlCommand cmd = new SqlCommand("select name_user,password_user from users where name_user='" + txtUsername.Text + "'and password_user='" + txtPassword.Text + "'", con);
+            SqlCommand cmd = new SqlCommand("select name_user,password_user from users where name_user='" + txtUsername.Text + "'and password_user='" + txtPassword.Text + "'", con.conectarbd);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             da.Fill(dt);
@@ -51,7 +49,7 @@ namespace Distribuidora
             {
                 MessageBox.Show("Nombre de usuario o contrasena incorrecto", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            con.Close();
+            con.cerrar();
         }
         private void chkPw_CheckedChanged(object sender, EventArgs e)
         {
@@ -67,12 +65,8 @@ namespace Distribuidora
 
         private void Login_Load(object sender, EventArgs e)
         {
-
-            SqlConnection con = new SqlConnection("Data Source=MANDO-NOTEBOOK;Initial Catalog=distribuidora;Integrated Security=True");
-            con.Open();
-
-            {
-            }
+            conexionbd conexion = new conexionbd();
+            conexion.abrir();
 
             this.FormClosed += new FormClosedEventHandler(CerrarApp);
 
